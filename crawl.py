33745dd23ac7f10ee7e7ca2ab4b6ead4376f6989
Noah Ingham <noahingham@gmail.com>
2013-11-14 18:53:43 +1100

    Database sorted out

--- crawl.py
+++ crawl.py
@@ -28,34 +28,60 @@ def delete(row):
     sql.execute("DELETE FROM links WHERE id=%s" %(row[0]))
 
 def update(row, field, value):
-    sql.execute("UPDATE links SET %s=%s WHERE id=%s" %(field, value, row[0]))
+    sql.execute("UPDATE links SET %s='%s' WHERE id='%s'" %(field, value, row))
 
 def select(field=id, value=False):
     if value:
         sql.execute("SELECT * FROM links WHERE %s='%s'" %(field, value))
     else:
         sql.execute("SELECT * FROM links")
-    return sql.fetchall()
+    resp = sql.fetchall()
+    if resp:
+        return resp[0]
+    else:
+        return ()
 
-def insert(url):
-    sql.execute("INSERT INTO links(url, parsed) VALUES('%s', 0)" %(url))
+def insert(url, linkedto=[], title='', description=''):
+    sql.execute("INSERT INTO links(url, parsed, linkedto, title, descr) VALUES('%s', 0, '%s', '%s', '%s')" %(url, linkedto, title, description))
+
 
 def email(address, site):
     sql.execute("INSERT INTO emails(address, site) VALUES('%s', '%s')" %(address, site))
+
+def keyword(word, url):
+    urlId = select('url', url)
+    sql.execute("INSERT INTO keywords(words, ids) VALUES('%s', '%s')" %(word, urlId))
+
+def incrementor():
+    sql.execute("SHOW TABLE STATUS LIKE 'links'")
+    return sql.fetchall()[0][10]
+
+def getId(url):
+    sel = select('url', url)
+    return sel[0]
+
+
+def reset():
+    sql.execute("DROP TABLE IF EXISTS links")
+    sql.execute("DROP TABLE IF EXISTS emails")
+    sql.execute("DROP TABLE IF EXISTS keywords")
+    sql.execute("CREATE TABLE links(id INT PRIMARY KEY AUTO_INCREMENT, url VARCHAR(256), parsed TINYINT(1), linkedto TEXT, title VARCHAR(128), descr VARCHAR(256))")
+    sql.execute("CREATE TABLE emails(id INT PRIMARY KEY AUTO_INCREMENT, address VARCHAR(256), site VARCHAR(256))")
+    sql.execute("CREATE TABLE keywords(word VARCHAR(40), ids TEXT)")
 
  # Connection to the server
 
 mysql = pymysql.connect(user='testpy', db='crawler')
 sql = mysql.cursor()
-
+reset()
+insert('http://google.com/', '0')
 
  # The initial URL to parse. Either a user-defined one or the first one in the table.
 
 if len(sys.argv)>1:
     start = sys.argv[1]
 else:
-    rows = select(parsed, 0)
-    start = select(parsed, 0)[0][1]
+    start = select(parsed, 0)[1]
 
 # -----END MYSQL BLOCK-----
 
@@ -86,13 +112,6 @@ def parse(url):
 
 
 
-
-
-
-
-
-
-
 # -----BEGIN CRAWLER BLOCK-----
 
 def checkUrl(url, link):
@@ -107,7 +126,7 @@ def checkUrl(url, link):
         url += '/'
     if url[7:11]=='www.':
         url = url[:7]+url[11:]
-    if url[8:11]=='www.':
+    if url[8:12]=='www.':
         url = url[:8]+url[12:]
     return url
 
@@ -116,25 +135,41 @@ def baseUrl(url):
         return url[:url.find('/',8)+1]
     else:
         return checkUrl(url[url.find('@')+1:], '')
-link = start
-linksl, title, keywords, description,h1 = parse(link)
 
+
+link = start
+start = select(parsed, 0)[1]
+linkId = getId(link)
+linksl, title, keywords, description,h1 = parse(link)
+for check in [title, keywords, description, h1]:
+    if not check:
+        check += ' '
 string = ""
 for x in (title + keywords + description + h1):
     string += ''.join([e for e in x.lower() if e.isalnum() or e==' '])
-string = list(set(string.split(' ')))
+string = [x for x in set(string.split(' ')) if x != '']
 
 links = []
 for l in linksl:
+    l = baseUrl(checkUrl(l, link))
     if l not in links:
         links.append(l)
-for url in links:
-    url=checkUrl(url, link)
-    print(baseUrl(url))
+for linkn in links:
+    li = select('url', linkn)
+    print('\t%s' %linkn)
+    if len(li) == 0:
+        insert(linkn, linkId)
+    else:
+        idh = li[0]
+        linkedto = li[3]
+        if link not in linkedto:
+            linkedto += ', ' + str(linkId)
+        update(idh, 'linkedto', linkedto)
+for key in string:
+    print(key)
+    #li = select('word', 'google')
 
 # -----END CRAWLER BLOCK-----
-
-
 
 
 
